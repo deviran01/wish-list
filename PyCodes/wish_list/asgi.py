3330aeaf4bc1233930781ec28ac602cb2cf74880
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2024-02-03 18:53:56 +0330

    :wrench: Initiate django config files

--- PyCodes/wish_list/asgi.py
+++ PyCodes/wish_list/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for wish_list project.
+ASGI config for PyCodes.wish_list project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wish_list.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PyCodes.wish_list.settings')
 
 application = get_asgi_application()
--- PyCodes/wish_list/wsgi.py
+++ PyCodes/wish_list/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for wish_list project.
+WSGI config for PyCodes.wish_list project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wish_list.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PyCodes.wish_list.settings')
 
 application = get_wsgi_application()
